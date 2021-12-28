6c66f4a46f38d3a2dd3c21026b82c4763243d6fe
mrbushik <mrbushik1@gmail.com>
2021-12-28 21:38:32 +0300

    not_adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,32 @@
-$(function(){
-    $(".rate-star").rateYo({
-        rating: 5,
-        starWidth: "12px",
-        readOnly: true
-    });
-    $(".product-slider__inner").slick({
-       dots: true,
-       arrows: false,
-        slidesToShow: 3,
-        slidesToScroll: 2
-    });
-    var mixer = mixitup('.products__inner-box');
-    
+// function init(){
+//     let map = new ymaps.map('map', {
+// center: [53.621447070907834,23.81796749999991],
+// zoom:16
+//     });
+// }
+// ymaps.ready(init);
+let center = [53.621447070907834,23.81796749999991];
+
+function init() {
+	let map = new ymaps.Map('map', {
+		center: center,
+		zoom: 17
+	});
+let placemark = new ymaps.Placemark(center,{},{
+iconLayout: 'default#image',
+iconImageHref: 'https://cdn-icons-png.flaticon.com/512/684/684908.png',
+iconImageSize: [25 , 25],
+iconImageOffset: [0 , 0]
 });
+	// map.controls.remove('geolocationControl'); // удаляем геолокацию
+  map.controls.remove('searchControl'); // удаляем поиск
+  map.controls.remove('trafficControl'); // удаляем контроль трафика
+//   map.controls.remove('typeSelector'); // удаляем тип
+//   map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+//   map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+  map.controls.remove('rulerControl'); // удаляем контрол правил
+  map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+  map.geoObjects.add(placemark);
+}
+
+ymaps.ready(init);
